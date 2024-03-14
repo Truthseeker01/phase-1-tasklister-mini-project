b7d96cf67006027225e761b1e49299254f9af39d
Ahmed Basamad <a.h.basamad.cn@gmail.com>
2024-03-14 13:11:23 -0400

    lab complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector("form").addEventListener("submit", e => {
+    e.preventDefault();
+    console.log("Task submitted!")
+    const taskSubmitted = document.querySelector("#new-task-description").value
+    console.log(taskSubmitted);
+    //const list = document.createElement("ul");
+    const arr = [taskSubmitted];
+    for (i=0; i < arr.length; i++){
+        const item = document.createElement("li");
+        item.appendChild(document.createTextNode(arr[i]));
+        document.querySelector("#tasks").appendChild(item);
+
+        const buttonElem = item.appendChild(document.createElement("button"));
+        buttonElem.innerHTML = "x";
+        buttonElem.onclick = function() {
+        item.remove();
+    }}
+  
+
+  })
+    
 });
